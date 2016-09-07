83fc51a9fd7589cf139aea576f8178e937104fc7
Rory Dent <whishana@gmail.com>
2016-09-07 11:18:24 +0200

    toggle test working

--- reducers/index.js
+++ reducers/index.js
@@ -1,24 +1,15 @@
 import * as webpackOptions from '../knowledge-base/webpackConfigOptions'
 
-const packages = (state = [], action) => {
-  switch (action.type) {
-    case 'TOGGLE_ITEM':
-  }
-}
-
 const packweb = (state = webpackOptions.initConfig, action) => {
   switch (action.type) {
     case 'TOGGLE_ITEM':
       let optionName = action.itemId
       let oldPackages = state.packages
       let newPackages = webpackOptions[optionName].packages
-      console.log('=============================')
-      console.log(...oldPackages)
-      console.log('=============================')
       let oldWebpackConfig = state.webpackConfig
       let newWebpackConfig = webpackOptions[optionName].webpackConfig
       return {
-        packages: oldPackages.map(p => packages(p, )),
+        packages: [...oldPackages,...newPackages],
         webpackConfig: {
           ...oldWebpackConfig,
           ...newWebpackConfig
