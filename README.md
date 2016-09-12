af1400c645f15eed0dd6b472a7bdbe09cf1e4226
Rory Dent <whishana@gmail.com>
2016-09-12 18:28:46 +0200

    modified readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # packweb
-desktop app made with electron for your webpack projects
+web-app to make a webpack config file easily
+
+## TODO :
+
+### Loaders :
+
+1. JS
+- [ ] ES6 (.js) or (.es6)
+- [ ] React (.js) or (.jsx)
+- [ ] CoffeScript (.coffee)
+- [ ] TypeScript (.ts)
+
+2. CSS
+- [ ] CSS
+- [ ] Sass
+- [ ] Less
+- [ ] Stylus
+
+3. Images
+- [ ] Image loaders ....
+- . . . .
+
+4. HTML
+
+### Plugins :
--- knowledge-base/webpackConfigOptions.js
+++ knowledge-base/webpackConfigOptions.js
@@ -1,5 +1,3 @@
-import escapeRegExp from 'lodash.escaperegexp'
-
 export const allPossibleOptions = [
   'devServer',
   'es6',
@@ -19,6 +17,10 @@ export const initPackagesByOption = {
   es6: ['babel-core', 'babel-preset-es2015', 'babel-loader'],
   react: ['babel-core', 'babel-preset-react', 'babel-loader']
 }
+
+// export const initImportsByPlugin = {
+//
+// }
 
 export const initConfigByOption = {
   'devServer': {
